f5ef66080f64330a9cb7df5d5b0b3dfac29d037f
rohitsinghwho <rohitsingh8655@gmail.com>
2026-02-27 22:16:30 +0530

    added dark mode button

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+import { CiSun } from "react-icons/ci";
+
+
 
 const NavBar = () => {
   return (
-    <header>
-      <nav className="w-full  min-h-7 text-center p-2 text-lg shadow shadow-olive-200 flex flex-row justify-center">
+    <header className="flex items-center">
+      <nav className="w-full  min-h-7 text-center p-2 text-lg shadow shadow-olive-200 flex flex-row justify-center items-center">
 
         {/* logo */}
         <span className="flex-1 text-2xl font-semibold text-blue-500">Shortyfy</span>
 
         {/* button */}
-        <div>
+        <div className="flex items-center justify-center">
             {/* TODO:DARK MODE - LIGHT MODE */}
-            dummy
+            <CiSun size={30} color="#D14009" />
         </div>
       </nav>
     </header>
